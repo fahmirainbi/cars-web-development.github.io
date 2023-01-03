a0cbd9eff775d559dddbf71e94e935d8328c0751
Fahmi Jamaludin <fahmijamaludin2@gmail.com>
2023-01-03 18:35:26 +0700

    edit page model

--- script.js
+++ script.js
@@ -34,6 +34,33 @@ function showSlide(n){
     slides[slideIndex - 1].style.display="block"
 }
 
+// slide model
+var slideIndex = 1;
+showSlide(slideIndex);
+
+function nextSlide (n){
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n){   
+    var i 
+    var slides = document.getElementsByClassName("color-slide")
+
+    if(n>slides.length){
+        slideIndex =1;
+    }
+    
+    if(n<1){
+        slideIndex = slides.length;
+    }
+
+    for(i=0;i<slides.length;i++){
+        slides[i].style.display="none"
+    }
+
+    slides[slideIndex - 1].style.display="block"
+}
+
 
 // slide
 
