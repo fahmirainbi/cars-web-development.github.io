b9d1b4f984aee108a27bd0123f47e8129549d6b5
Fahmi Jamaludin <fahmijamaludin2@gmail.com>
2023-01-05 22:28:58 +0700

    adding link

--- script.js
+++ script.js
@@ -79,3 +79,20 @@ var swiper = new Swiper(".slide-content", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  var swiper = new Swiper(".slide-content-model", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
