ec1ed3bab518a406d121e1863763aa1657000240
Fahmi Jamaludin <fahmijamaludin2@gmail.com>
2023-01-09 03:57:10 +0700

    final

--- script.js
+++ script.js
@@ -6,33 +6,13 @@ menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 })
 
-// carosel 
+// model menu
+const model = document.querySelector('.dropdwn input');
+const dropdown = document.querySelector('.dropdwn .dropdown-content-model');
 
-var slideIndex = 1;
-showSlide(slideIndex);
-
-function nextSlide (n){
-    showSlide(slideIndex += n);
-}
-
-function showSlide(n){   
-    var i 
-    var slides = document.getElementsByClassName("content-slide")
-
-    if(n>slides.length){
-        slideIndex =1;
-    }
-    
-    if(n<1){
-        slideIndex = slides.length;
-    }
-
-    for(i=0;i<slides.length;i++){
-        slides[i].style.display="none"
-    }
-
-    slides[slideIndex - 1].style.display="block"
-}
+model.addEventListener('click', function(){
+    dropdown.classList.toggle('open');
+})
 
 // slide model
 var slideIndex = 1;
@@ -66,33 +46,30 @@ function showSlide(n){
 
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
+    spaceBetween: 25,
     loop: true,
-    loopFillGroupWithBlank: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      dynamicBullets: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
 
-
-  var swiper = new Swiper(".slide-content-model", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
     },
   });
